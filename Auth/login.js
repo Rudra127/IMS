eded07dfaae18b6daeaa961d7d4b2783ae0945e8
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-10-17 23:09:21 +0530

    deploy

--- Auth/login.js
+++ Auth/login.js
@@ -7,27 +7,15 @@ const loginUsers = async (req, res) => {
     const existUser = await registerUsers.findOne({ email });
 
     if (existUser) {
-      
-
-    if (password === existUser.password) {
-
-      res.status(200).json({ message: "User logged in successfully" });
-
-   
-    }
-
-    else{
-      // If the user exists and the password is correct, you can consider the user logged in
-      res.status(401).json({ message: "Incorrect password" });
-
-
-    }
-    }
-    else{
-      
+      if (password === existUser.password) {
+        res.status(200).json({ message: "User logged in successfully" });
+      } else {
+        // If the user exists and the password is correct, you can consider the user logged in
+        res.status(401).json({ message: "Incorrect password" });
+      }
+    } else {
       res.status(404).json({ message: "User not found" });
-    
-  }
+    }
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).json({ message: "Internal server error" });
